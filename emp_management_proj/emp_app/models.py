7714a535e5e0dc9ceccba35821858158ce7df08a
Gaurav Sonawane <vgauravs2001@gmail.com>
2023-03-20 15:01:31 +0530

    add page view all employees

--- emp_management_proj/emp_app/models.py
+++ emp_management_proj/emp_app/models.py
@@ -6,17 +6,26 @@ class Department(models.Model):
     name = models.CharField(max_length=100, null=False)
     location = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 
 class Role(models.Model):
     name = models.CharField(max_length=100, null=False)
+
+    def __str__(self):
+        return self.name
 
 
 class Employee(models.Model):
     first_name = models.CharField(max_length=100, null=False)
     last_name = models.CharField(max_length=100)
-    dept = models.CharField(max_length=100)
-    salary = models.ForeignKey(Department, on_delete=models.CASCADE)
+    dept = models.ForeignKey(Department, on_delete=models.CASCADE)
+    salary = models.CharField(max_length=100)
     bonus = models.IntegerField(default=0)
     role = models.ForeignKey(Role, on_delete=models.CASCADE)
     phone = models.IntegerField(default=0)
     hire_date = models.DateField()
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name} {self.phone}'
--- emp_management_proj/emp_app/views.py
+++ emp_management_proj/emp_app/views.py
@@ -1,6 +1,28 @@
 from django.shortcuts import render
+from .models import Employee, Role, Department
 
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+
+def all_emp(request):
+    emps = Employee.objects.all()
+    context = {
+        'emps': emps
+    }
+    return render(request, 'view_all_emp.html', context)
+
+
+def add_emp(request):
+
+    return render(request, 'add_emp.html')
+
+
+def remove_emp(request):
+    return render(request, 'remove_emp.html')
+
+
+def filter_emp(request):
+    return render(request, 'filter_emp.html')
